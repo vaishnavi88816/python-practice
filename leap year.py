1c8a940b60467c5344a84fa6c0817d1ec5305026
vaishnavi88816 <guptavaishnavi0706@gmail.com>
2026-03-28 10:59:36 +0530

    leap year
    add a new file of leap year

---
+++ leap year.py
@@ -0,0 +1,13 @@
+year = int(input("Enter a year: "))
+
+if year % 400 == 0:
+    print("Leap Year")
+
+elif year % 100 == 0:
+    print("Not a Leap Year")
+
+elif year % 4 == 0:
+    print("Leap Year")
+
+else:
+    print("Not a Leap Year")
