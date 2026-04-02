5d97430b18923859c4d704f0ab2ccb5153a3b629
vaishnavi88816 <guptavaishnavi0706@gmail.com>
2026-04-02 08:29:55 +0530

    add minmax program using array

---
+++ minmax.py
@@ -0,0 +1,5 @@
+import numpy as np
+arr = np.array([12,45,67,54,32])
+print("Minimum no of the array:",np.min(arr))
+print("Maximum no of the array:",np.max(arr))
+print("Mean no of the array:",np.mean(arr))
