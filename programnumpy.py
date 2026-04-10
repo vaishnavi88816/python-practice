f35221ae282170e240406d0a664cb0c9a3911809
vaishnavi88816 <guptavaishnavi0706@gmail.com>
2026-04-10 10:32:48 +0530

    program using numpy

---
+++ programnumpy.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# Create two arrays
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+# Addition
+sum_array = a + b
+
+# Multiplication
+mul_array = a * b
+
+# Mean of array a
+mean_a = np.mean(a)
+
+print("Array a:", a)
+print("Array b:", b)
+print("Addition:", sum_array)
+print("Multiplication:", mul_array)
+print("Mean of a:", mean_a)
