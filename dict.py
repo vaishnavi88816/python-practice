6c6c47d28b9f673828892ba2f8cb65af4bc7a2c0
vaishnavi88816 <guptavaishnavi0706@gmail.com>
2026-04-14 08:29:01 +0530

    Added user defined dictionary program

---
+++ dict.py
@@ -0,0 +1,9 @@
+d1={}
+n=int(input("print a number of dictionary u want to add:"))
+i=1
+while i<=n:
+    key_value=int(input("print a key value:"))
+    data_value=input("enter a data value")
+    d1[key_value]=data_value
+    i+=1
+print(d1)
