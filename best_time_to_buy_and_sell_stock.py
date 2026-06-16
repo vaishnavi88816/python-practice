14c44d97d90ccb4f3d66ccfbf6cd21491bc44e95
vaishnavi88816 <guptavaishnavi0706@gmail.com>
2026-06-16 21:58:13 +0530

    best_time_to_buy_and_sell_stock
    add a new file of buy and sell stock

---
+++ best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def maxProfit(self, prices):
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+
+            profit = price - min_price
+
+            if profit > max_profit:
+                max_profit = profit
+
+        return max_profit
+obj=Solution()
+print(obj.maxProfit([7,1,5,3,6,4]))
