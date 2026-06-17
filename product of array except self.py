bab958090569ce7aebad7e882348e018893e31b6
vaishnavi88816 <guptavaishnavi0706@gmail.com>
2026-06-17 23:38:09 +0530

    Product_of_array_except_self
    add a new program of Product_of_array_except_self

---
+++ product of array except self.py
@@ -0,0 +1,23 @@
+class Solution:
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        answer = [1] * n
+
+        prefix = 1
+        for i in range(n):
+            answer[i] = prefix
+            prefix *= nums[i]
+
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            answer[i] *= suffix
+            suffix *= nums[i]
+
+        return answer
+
+
+obj = Solution()
+
+nums = [1, 2, 3, 4]
+
+print(obj.productExceptSelf(nums))
