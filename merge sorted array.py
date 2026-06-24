7645095d4970c188bd5537e10fb0bdae61906c43
vaishnavi88816 <guptavaishnavi0706@gmail.com>
2026-06-24 23:57:30 +0530

    Merge Sorted Array
    Add a new program of merge sorted array

---
+++ merge sorted array.py
@@ -0,0 +1,29 @@
+
+
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+
+nums1 = [1,2,3,0,0,0]
+nums2 = [2,5,6]
+
+Solution().merge(nums1, 3, nums2, 3)
+print(nums1)
